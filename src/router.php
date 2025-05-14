b3df73a68263da962c35724cf6275f0153014837
Guglielmo Frati <guglielmo.frati@codingduck.it>
2025-05-14 09:24:53 +0000

    Init

---
+++ src/router.php
@@ -0,0 +1,16 @@
+<?php
+// Create Router instance
+$router = new \Bramus\Router\Router();
+
+$router->get('/', function() {
+    echo json_encode([
+        "message" => "Hello world",
+    ]);
+});
+
+$router->before('GET|POST|PUT|DELETE', '/.*', function() {
+    header('Content-Type: application/json');
+});
+
+// Run it!
+$router->run();
