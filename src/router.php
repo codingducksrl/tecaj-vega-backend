fc3d07cb1d43de146926f8185b4b5fb9a5508a59
marcel695 <marcel.gospodaric@potep.org>
2025-05-30 13:01:29 +0200

    vsi API klici koncani

--- src/router.php
+++ src/router.php
@@ -4,34 +4,35 @@ $router = new \Bramus\Router\Router();
 
 $router->setNamespace("Vscode\TecajVegaBackend\Controllers");
 $router->get('/test',"HelloController@hello");
+$router->get('/refresh',"RefreshController@hello");
 
 $router->get('/', function() {
 
-    $client = new GuzzleHttp\Client();
+    // $client = new GuzzleHttp\Client();
 
-    $query = array();
-    parse_str($_SERVER["QUERY_STRING"], $query);
+    // $query = array();
+    // parse_str($_SERVER["QUERY_STRING"], $query);
 
 
-    $res = $client->request('GET', 'https://store.steampowered.com/api/appdetails', [
-        "query"=>[
-        "appids" => "570"
-        ]
-    ]);
+    // $res = $client->request('GET', 'https://store.steampowered.com/api/appdetails', [
+    //     "query"=>[
+    //     "appids" => "570"
+    //     ]
+    // ]);
 
-    $podatki = json_decode((string)$res->getBody(), true);
+    // $podatki = json_decode((string)$res->getBody(), true);
 
-    echo json_encode([
-        "message" => "Hello world",
-        "status" => $res->getStatusCode(),
-        "name" => $podatki["570"]["data"]["name"],
-        "category" => $podatki["570"]["data"]["categories"],
-        "genre" => $podatki["570"]["data"]["genres"],
-        "short_description" => $podatki["570"]["data"]["short_description"],
-        "header_image" => $podatki["570"]["data"]["header_image"],
-        "link" => "https://store.steampowered.com/app/570",
-        "query" => $query
-    ]);
+    // echo json_encode([
+    //     "message" => "Hello world",
+    //     "status" => $res->getStatusCode(),
+    //     "name" => $podatki["570"]["data"]["name"],
+    //     "category" => $podatki["570"]["data"]["categories"],
+    //     "genre" => $podatki["570"]["data"]["genres"],
+    //     "short_description" => $podatki["570"]["data"]["short_description"],
+    //     "header_image" => $podatki["570"]["data"]["header_image"],
+    //     "link" => "https://store.steampowered.com/app/570",
+    //     "query" => $query
+    // ]);
 });
 
 $router->before('GET|POST|PUT|DELETE', '/.*', function() {
