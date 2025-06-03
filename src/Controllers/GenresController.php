a20114ade4d9bd6a86bad97b0d9d646f790dd653
PozziIlBello <peter.pozzar@potep.org>
2025-06-03 11:55:57 +0200

    Dobi id igre glede na genre

--- src/Controllers/GenresController.php
+++ src/Controllers/GenresController.php
@@ -8,14 +8,28 @@ use Vscode\TecajVegaBackend\Models\Uporabnik_ima_kategorije;
 class GenresController {
 
     public static function genres($STEAM_ID) {
-
         echo json_encode(
             Uporabnik_ima_kategorije::get($STEAM_ID)
         );
     }
 
-    public static function reccomendations($_STEAM_ID, $GENRES) {
+    public static function reccomendations($GENRES) {
+        $client = new \GuzzleHttp\Client();
+            $igre_najbolj_igrane = $client->request('GET', 'https://store.steampowered.com/api/getappsingenre/', [
+                "query"=>[
+                    "genre" => $GENRES,
+                ]
+            ]);
+            
+            $podatki = json_decode((string)$igre_najbolj_igrane->getBody(), true); // dekodiramo podatke
 
+            for( $i = 0; $i < count($podatki["tabs"]["newreleases"]["items"]); $i++) {
+                echo json_encode([
+                    "idNewrelese" => $podatki["tabs"]["newreleases"]["items"][$i]["id"],
+                    "idTopseller" => $podatki["tabs"]["topsellers"]["items"][$i]["id"],
+                ]);
+            
+            }
     }
 
 }
